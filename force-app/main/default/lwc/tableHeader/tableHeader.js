9768414e4234aeb1912b5bd28fa5074c55486093
dmitry.kondar <dmitry.kondar@speedandfunction.com>
2022-06-23 12:36:14 +0300

    integrate prompt to table headers

--- force-app/main/default/lwc/tableHeader/tableHeader.js
+++ force-app/main/default/lwc/tableHeader/tableHeader.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import LightningPrompt from "lightning/prompt";
 
-export default class TableHearder extends LightningElement {}
+export default class TableHearder extends LightningElement {
+  handlePrompt() {
+    LightningPrompt.open({
+      theme: "inverse",
+      label: "Type Header text",
+      defaultValue: "Test"
+    }).then((result) => {
+      console.log("🚀 ~ result", result);
+    });
+  }
+}
